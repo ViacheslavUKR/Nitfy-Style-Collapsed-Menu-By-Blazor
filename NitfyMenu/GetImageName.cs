615cf86565f4e24903f0e587cfec4646f72844c5
burgas275 <burgas275@gmail.com>
2020-06-29 03:28:57 +0300

    first commit

---
+++ NitfyMenu/GetImageName.cs
@@ -0,0 +1,19 @@
+﻿using NitfyMenu.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NitfyMenu
+{
+    public class ImageName
+    {
+        public static string Get()
+        {
+            if (string.IsNullOrEmpty(Breadcrumb.curPage))
+                return "premium/boxed-bg/abstract/bg/1.jpg";
+            else
+                return $"premium/boxed-bg/abstract/bg/{ Breadcrumb.curPage.Replace("Page", "")}.jpg";
+        }
+    }
+}
