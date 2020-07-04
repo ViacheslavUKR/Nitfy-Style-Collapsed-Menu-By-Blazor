87c20d9de24e29764faaf37f8b01b7b3391c905e
burgas275 <burgas275@gmail.com>
2020-07-04 23:21:26 +0300

    Nitfy-style collapsed menu with AspNetCore.Identity

---
+++ NitfyMenu/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Models;
+
+namespace Accounts.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IDataProtector _dataProtector;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public AccountController(IDataProtectionProvider dataProtectionProvider, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IWebHostEnvironment hostingEnvironment)
+        {
+            _dataProtector = dataProtectionProvider.CreateProtector("SignIn");
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("account/signinactual")]
+        public async Task<IActionResult> SignInActual(string t)
+        {
+            var data = _dataProtector.Unprotect(t);
+
+            var parts = data.Split('|');
+
+            var identityUser = await _userManager.FindByIdAsync(parts[0]);
+
+            var isTokenValid = await _userManager.VerifyUserTokenAsync(identityUser, TokenOptions.DefaultProvider, "SignIn", parts[1]);
+
+            if (isTokenValid)
+            {
+                await _signInManager.SignInAsync(identityUser, true);
+                if (parts.Length == 3 && Url.IsLocalUrl(parts[2]))
+                {
+                    return Redirect(parts[2]);
+                }
+                return Redirect("/");
+            }
+            else
+            {
+                return Unauthorized("STOP!");
+            }
+        }
+
+        [Authorize]
+        [HttpGet("account/signout")]
+        public async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            return Redirect("/Logoff");
+        }
+    }
+}
--- NitfyMenu/Startup.cs
+++ NitfyMenu/Startup.cs
@@ -8,44 +8,82 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using NitfyMenu;
 
 namespace NitfyMenu
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<ApplicationDbContext>(
+    options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")),
+    ServiceLifetime.Transient);
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 4;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+
+                options.SignIn.RequireConfirmedEmail = true;
+
+            }).AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdmin", c => c.RequireRole("Admin"));
+            });
+
+            services.AddHttpClient();
+            services.AddHttpContextAccessor();
+
             services.AddRazorPages();
+            services.AddControllers();
             services.AddServerSideBlazor();
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DbContextOptions<ApplicationDbContext> identityDbContextOptions, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseHsts();
             }
 
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
